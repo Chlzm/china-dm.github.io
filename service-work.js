c280495252f2064930f72c4f84d9eb26b79c5470
liuzhimin <liuzhimin@gaodun.com>
2018-10-15 14:31:57 +0800

    save

---
+++ service-work.js
@@ -0,0 +1,14 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-1'
+var cacheList=[
+    '/',
+    'index.html',
+    'css/main1.css',
+]
+self.addEventListener('install',e =>{
+    e.waitUntil(
+        caches.open(cacheStorageKey)
+            .then(cache => cache.addAll(cacheList))
+            .then(() => self.skipWaiting())
+    )
+})
