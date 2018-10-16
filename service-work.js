224c0b91ceee743ba781bf64da67028b8d84e5ff
liuzhimin <liuzhimin@gaodun.com>
2018-10-16 15:32:52 +0800

    save

--- service-work.js
+++ service-work.js
@@ -1,14 +1,130 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-1'
-var cacheList=[
-    '/',
-    'index.html',
-    'css/main1.css',
-]
-self.addEventListener('install',e =>{
-    e.waitUntil(
-        caches.open(cacheStorageKey)
-            .then(cache => cache.addAll(cacheList))
-            .then(() => self.skipWaiting())
-    )
+
+
+self.addEventListener('error', function(e) {
+    self.clients.matchAll()
+        .then(function (clients) {
+            if (clients && clients.length) {
+                clients[0].postMessage({
+                    type: 'ERROR',
+                    msg: e.message || null,
+                    stack: e.error ? e.error.stack : null
+                });
+            }
+        });
+});
+
+self.addEventListener('unhandledrejection', function(e) {
+    self.clients.matchAll()
+        .then(function (clients) {
+            if (clients && clients.length) {
+                clients[0].postMessage({
+                    type: 'REJECTION',
+                    msg: e.reason ? e.reason.message : null,
+                    stack: e.reason ? e.reason.stack : null
+                });
+            }
+        });
 })
+
+importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
+workbox.setConfig({
+    debug: false,
+    modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/'
+});
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+var cacheList = [
+    '/',
+    '/manifest.json'
+];
+
+workbox.routing.registerRoute(
+    function(event) {
+        // 需要缓存的HTML路径列表
+        if(~cacheList.indexOf(event.url.pathname)){
+            return true
+        }
+        return false;
+        /*if (event.url.host === '/') {
+        //if (event.url.host === 'www.taobao.com') {
+            if (~cacheList.indexOf(event.url.pathname)) return true;
+            else return false;
+        } else {
+            return false;
+        }*/
+    },
+    workbox.strategies.networkFirst({
+        cacheName: 'tbh:html',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 10
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.css/,
+    //new RegExp('https://g\.alicdn\.com/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'tbh:static',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 20
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.(png|jpg|jpeg|gif)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'tbh:img',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 20,
+                maxAgeSeconds: 12 * 60 * 60
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://gtms01\.alicdn\.com/'),
+    workbox.strategies.cacheFirst({
+        cacheName: 'tbh:img',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 30,
+                maxAgeSeconds: 12 * 60 * 60
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    /https:\/\/wwwupload\.gaodunwangxiao\.com\/images\/rgtConsult\.png/,
+    workbox.strategies.networkFirst({
+        cacheName: 'cros:img',
+        cacheExpiration: {
+            maxEntries: 10,
+            maxAgeSeconds: 7 * 24 * 60 * 60
+        },
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.Plugin({
+                maxEntries: 30,
+                maxAgeSeconds: 12 * 60 * 60
+            })
+        ]
+    })
+)
